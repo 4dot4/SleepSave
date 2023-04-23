ee4649c7848118943be1fc2787e4c4c845743c81
4dot4 <controlgame@icloud.com>
2023-04-23 12:00:55 -0300

    added the buttons, added the endtime and some other labels

--- main.go
+++ main.go
@@ -1,27 +1,41 @@
 package main
 
 import (
-	//"fmt"
 	"fmt"
-	gui "github.com/gen2brain/raylib-go/raygui"
 	"time"
+
+	gui "github.com/gen2brain/raylib-go/raygui"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
-type Timer struct {
-	Hours   int
-	Minutes int
-}
-
 func main() {
 	rl.InitWindow(800, 800, "SleepSave")
-	var timer Timer
+	var timer time.Duration
+	fmt.Println(timer)
 	for !rl.WindowShouldClose() {
 		var clock = time.Now()
+		var endtime = clock.Add(timer)
 		rl.BeginDrawing()
+
 		rl.ClearBackground(rl.Black)
-		rl.DrawText(clock.Format("03:04 am"), 250, 100, 60, rl.Blue)
+		if gui.LabelButton(rl.Rectangle{300, 400, 20, 20}, "+ 1 hour") {
+			timer += time.Hour
+		}
+		if gui.LabelButton(rl.Rectangle{300, 600, 20, 20}, "- 1 hour") {
+			timer -= time.Hour
+		}
+		if gui.LabelButton(rl.Rectangle{400, 400, 20, 20}, "+ 30 minutes") {
+			timer += 30 * time.Minute
+		}
+		if gui.LabelButton(rl.Rectangle{400, 600, 20, 20}, "- 30 minutes") {
+			timer -= 30 * time.Minute
+		}
+		rl.DrawText("it's "+clock.Format(time.Kitchen), 250, 100, 60, rl.Blue)
+		rl.DrawText("End Time "+endtime.Format(time.Kitchen), 100, 700, 60, rl.Red)
+
+		format := fmt.Sprintf("%v", timer)
+		rl.DrawText(format, 300, 500, 60, rl.Green)
 		rl.EndDrawing()
 	}
 }
